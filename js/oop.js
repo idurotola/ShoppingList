9ca885509c7843b7bb187b32e9d6f022f9056600
tommiedure <megamee2011@gmail.com>
2014-09-10 10:18:37 +0100

    initialising repo for shopping list GH Pages

---
+++ js/oop.js
@@ -0,0 +1,50 @@
+var shoppingList = {
+
+
+		initialize : function () {
+			$('button').click(shoppingList.addItemBtn);
+			$(".clickAble").click(shoppingList.listEvent);
+			$('.initialList ul').on('click','li a',shoppingList.rmvItem);
+			$('.initialList ul').on('change','[type=checkbox]',shoppingList.finalAdd);
+			$('.finalList ul').on('change','[type=checkbox]',shoppingList.initialAdd);
+			$('.dropDown').click(shoppingList.hideList);
+
+		},
+
+		addItemBtn : function(){    //this adds item  to the initial list from the input
+			var addObj = $.trim($(".inputField").val());
+			if(addObj==""||addObj.length < 2) {
+				alert("Please Enter A Valid Item Name");
+			}
+			else {
+			$(".initialList ul").append('<li><input id rel="checked" type="checkbox" value=addObj />' + addObj + '<a class="clickAble item" id="del">Delete</a></li>');
+					}
+				},
+
+
+
+		listEvent : function(){		//this adds item  to the initial list from the listed suggestions
+			var addObj = $(this).attr("value");
+			$(".initialList ul").append('<li><input id rel="checked" type="checkbox" value=addObj />' + addObj + '<a class="clickAble item" id="del">Delete</a></li>');
+								},
+
+		rmvItem : function(){		//this deletes item  from the initial list 
+	    		$(this).parent().hide('slow', function(){ $(this).remove(); });
+	    						},
+
+		finalAdd : function(){
+					var toMove = $(this).parent();
+	    				$('.finalList ul').append(toMove);
+								},
+
+		initialAdd  : function(){
+					var toMove = $(this).parent();
+					$('.initialList ul').append(toMove);
+								},
+
+		hideList : function() {
+				$(".listItems li:not(:last)").fadeToggle('fast');
+								}
+};
+
+$(document).ready(shoppingList.initialize);
